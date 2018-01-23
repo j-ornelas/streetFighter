960019661decd67e0fc2b429eb4b3818e4ce710d
Fred Zirdung <fred@hackreactor.com>
2018-01-23 12:30:57 -0800

    Solution Code

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,8 +5,9 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
-  });
+
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
+      });
 
   it('should have a jQuery $node object', function() {
     expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
@@ -22,8 +23,8 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-      clock.tick(timeBetweenSteps);
+
+            clock.tick(timeBetweenSteps);
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
